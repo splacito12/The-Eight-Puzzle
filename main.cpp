88d2e3ffe5e0d488161d103d00c17fb693958c2d
Samantha Placito Melendrez <95974510+splacito12@users.noreply.github.com>
2026-02-07 02:08:17 +0000

    Finished the A* functions. Working on the Uniform Cost Search

--- main.cpp
+++ main.cpp
@@ -31,7 +31,7 @@ vector<int> goalState = {1, 2, 3, 4, 5, 6, 7, 8, 0};
 //Here we will be creating comparing our priority queue for A* 
 struct comparePQ{
     bool operator()(gameState x, gameState y){
-        return (x.g(n) + x.h(n)) > (y.g(n) + y.h(n));
+        return (x.g(n) + x.h(n)) > (y.g(n) + y.h(n)); //since we are doing PQ, we want the lowest value
     }
 }
 
@@ -45,6 +45,55 @@ bool goalTest(const vector<int>& game){
     All of our search related functions will go here
 
 */
+
+//the A* misplaced tile heuristic function
+int calculateMiplaced(const vector<int>& game){
+    int misplacedTile = 0;
+    for(int i = 0; i < 9; i++){
+        //check whether the tile is in its position. Increment if not.
+        if(game[i] != 0 && game[i] != goalState[i]){
+            misplacedTile++;
+        }
+    }
+    return misplacedTile;
+}
+
+//the A* manhattan didistance heuristic function
+int calculateManhattan(const vector<int>& game){
+    //since we are doing manhattan distance, we will need to calculate the distance
+    int distSum = 0;
+    for(int i = 0; i < 9; i++){
+        //calculate the distance of the tiles that aren't in their position
+        if(game[i] == 0){ 
+            continue;
+        }else{
+            //calculate the goal row and column
+            //remember, we have three rows and three columns
+            int Row = (game[i] - 1) / 3;
+            int Column = (game[i] - 1) % 3;
+
+            //now, we need to calculate the current row and column
+            int currRow = i / 3;
+            int currColumn = i % 3;
+
+            //calculate the absolute value of the two distanced
+            distSum += abs(Row - currRow) + abs(Column - currColumn);
+        }
+    }
+
+    return distSum;
+}
+
+//uniform cost search function
+void UCS(const vector<int>& startState){
+    //remember we use priority queue for uniform cost search
+    //we need a function for the all of the nodes we visited
+    priority_queue<gameState, vector<gameState>, comparePQ> priorityQueue;
+
+}
+
+
+
 
 //Now, we will be printing ou our game board
 void printGameBoard(const vector<int>& game){
